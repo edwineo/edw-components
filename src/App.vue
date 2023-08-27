63a4e4dacdc3e7c4c1d12dbdb7f9028310c63ca6
liu chang <aboutcroon@gmail.com>
2023-08-27 15:45:41 +0800

    feat: FinanceForm finish

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/form-table">FormTable</RouterLink>
+        <RouterLink to="/finance-form">FinanceForm</RouterLink>
       </nav>
     </div>
   </header>
